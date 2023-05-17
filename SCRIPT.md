7996ffefc0a78b7213c12239574d5b8033a04cb8
Davi Borges <davi.borges.developer@gmail.com>
2023-05-17 12:59:08 -0300

    aula2

--- SCRIPT.md
+++ SCRIPT.md
@@ -1,1 +1,3 @@
 flutter create --project-name delivery_backoffice_dw10 --org br.com.academiadoflutter --platforms web ./delivery_backoffice_dw10
+
+dart run build_runner watch -d
--- lib/src/modules/home/home_page.dart
+++ lib/src/modules/home/home_page.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  const HomePage({ super.key });
-
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('Home Page'),),
-           body: Container(),
-       );
+  @override
+  Widget build(BuildContext context) {
+    return const Text('HomePage');
   }
 }
